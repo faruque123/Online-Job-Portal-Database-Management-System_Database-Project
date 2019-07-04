94e7ac278ea308005c11b831688d556f3d004551
faruque123 <52509389+faruque123@users.noreply.github.com>
2019-07-04 10:32:17 +0600

    Add files via upload

---
+++ trainingform.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+</head>
+<body bgcolor = "#89BDFF">
+<form action="addtraining.php" method="post">
+Course Type:<br> <input type="text" name="ct"> <br>
+Company Name: <br><input type="text" name="company"><br>
+location: <br><input type="text" name="location"><br>
+Start date: <br><input type="date" name="st_date" value = "dd-mm-yyy">  <br>
+End date: <br><input type="date" name="end_date" value = "dd-mm-yyy"><br>
+<br>
+Schedule:<br>
+Class Location:<br> <input type="text" name="cl"> <br>
+Start Time: <br><input type="text" name="starttime"><br>
+End Time: <br><input type="text" name="endtime"><br>
+Class Date: <br><input type="text" name="c_date" value = "dd-mm-yyy">  <br>
+
+<input type="submit">
+</form>
+</body>
+</html>
